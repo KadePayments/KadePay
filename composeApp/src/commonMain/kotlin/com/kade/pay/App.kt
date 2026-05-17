c155e4001f475723871d9a59ce2e55af820f3ef9
Shubert Munthali <shubertm.m@gmail.com>
2026-05-17 13:17:20 +0200

    Add MainScreen

--- composeApp/src/commonMain/kotlin/com/kade/pay/App.kt
+++ composeApp/src/commonMain/kotlin/com/kade/pay/App.kt
@@ -1,53 +1,14 @@
 package com.kade.pay
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kadepay.composeapp.generated.resources.Res
-import kadepay.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import com.kade.pay.presentation.screens.MainScreen
+import com.kade.pay.presentation.theme.KadePayTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier =
-                Modifier
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .safeContentPadding()
-                    .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    KadePayTheme(false) {
+        MainScreen()
     }
 }
--- composeApp/src/jvmMain/kotlin/com/kade/pay/Main.kt
+++ composeApp/src/jvmMain/kotlin/com/kade/pay/Main.kt
@@ -2,12 +2,16 @@ package com.kade.pay
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kadepay.composeapp.generated.resources.Res
+import kadepay.composeapp.generated.resources.kade
+import org.jetbrains.compose.resources.painterResource
 
 fun main() =
     application {
         Window(
             onCloseRequest = ::exitApplication,
             title = "KadePay",
+            icon = painterResource(Res.drawable.kade),
         ) {
             App()
         }
