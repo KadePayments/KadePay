baef03767e900b00f0f70fac64574dedc0f4d0d4
Shubert Munthali <shubertm.m@gmail.com>
2026-07-12 19:33:05 +0200

    Derive HTTP protocol from network

--- composeApp/src/jvmMain/kotlin/com/kade/pay/network/GRPCClient.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/kade/pay/network/GRPCClient.jvm.kt
@@ -5,16 +5,24 @@ import okhttp3.OkHttpClient
 import okhttp3.Protocol
 import kotlin.time.Duration.Companion.seconds
 
-actual fun grpcClient(config: Config): GrpcClient =
-    GrpcClient
+actual fun grpcClient(config: Config): GrpcClient {
+    val httpProtocol =
+        when (config) {
+            Config.MAINNET -> Protocol.HTTP_2
+            Config.SIGNET -> Protocol.HTTP_2
+            Config.TESTNET -> Protocol.HTTP_2
+            Config.REGTEST -> Protocol.H2_PRIOR_KNOWLEDGE
+        }
+    return GrpcClient
         .Builder()
         .client(
             OkHttpClient
                 .Builder()
-                .protocols(listOf(Protocol.H2_PRIOR_KNOWLEDGE))
+                .protocols(listOf(httpProtocol))
                 .callTimeout(60.seconds)
                 .readTimeout(30.seconds)
                 .connectTimeout(30.seconds)
                 .build(),
         ).baseUrl(config.kadePayUrl)
         .build()
+}
