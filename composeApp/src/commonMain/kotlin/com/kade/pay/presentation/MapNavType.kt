2384ad66d43051aa8acb55baf6ae93b75b269506
Shubert Munthali <shubertm.m@gmail.com>
2026-08-07 21:44:42 +0200

    Serialize maps for navigation

---
+++ composeApp/src/commonMain/kotlin/com/kade/pay/presentation/MapNavType.kt
@@ -0,0 +1,39 @@
+package com.kade.pay.presentation
+
+import androidx.navigation.NavType
+import androidx.savedstate.SavedState
+import androidx.savedstate.read
+import androidx.savedstate.write
+
+object MapNavType : NavType<Map<String, String>>(false) {
+    override fun put(
+        bundle: SavedState,
+        key: String,
+        value: Map<String, String>,
+    ) {
+        bundle.write {
+            val stringList =
+                value.map { entry ->
+                    "${entry.key}:${entry.value}"
+                }
+            putStringList(key, stringList)
+        }
+    }
+
+    override fun get(
+        bundle: SavedState,
+        key: String,
+    ): Map<String, String> =
+        bundle.read {
+            getStringList(key).associate { item ->
+                val (itemKey, value) = item.trim().split(":")
+                itemKey to value
+            }
+        }
+
+    override fun parseValue(value: String): Map<String, String> =
+        value.split(",").associate { item ->
+            val (itemKey, value) = item.trim().split(":")
+            itemKey to value
+        }
+}
